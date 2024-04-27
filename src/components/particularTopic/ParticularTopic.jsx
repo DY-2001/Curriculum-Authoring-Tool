d24a294865882369bc2b7e1b5804b59e0ecc8ac8
DY-2001 <dushyant.y20@iiits.in>
2024-04-27 18:38:15 +0530

    tooltip for actions

--- src/components/particularTopic/ParticularTopic.jsx
+++ src/components/particularTopic/ParticularTopic.jsx
@@ -5,10 +5,10 @@ const ParticularTopic = () => {
     <div className={styles["topicStyles"]}>
       <div className={styles["topicActionAndName"]}>
         <div className={styles["actionContainer"]}>
-            <img src="/images/move.png" className={styles["actionIcon"]} />
-            <img src="/images/back.png" className={styles["actionIcon"]} />
-            <img src="/images/next.png" className={styles["actionIcon"]} />
-            <img src="/images/bin.png" className={styles["actionIcon"]} />
+            <img src="/images/move.png" title="move" className={styles["actionIcon"]} />
+            <img src="/images/back.png" title="outdend" className={styles["actionIcon"]} />
+            <img src="/images/next.png" title="indent" className={styles["actionIcon"]} />
+            <img src="/images/bin.png" title="delete" className={styles["actionIcon"]} />
         </div>
         <div className={styles["actionContainer"]}>
           <p>Standard</p>
