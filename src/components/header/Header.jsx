edd09e8f1622ba47eb0535d72543673c259725bd
DY-2001 <dushyant.y20@iiits.in>
2024-04-29 04:21:16 +0530

    save functionality

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,49 @@
 import styles from "./Header.module.css";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Line from "../line/Line";
+import { TopicContext } from "../../App";
 
 const Header = () => {
   const [subject, setSubject] = useState("MATHEMATICS");
+  const { topicsData } = useContext(TopicContext);
+
+  const handleCreateData = (data) => {
+    let newData = "Curriculum";
+    for (let i = 0; i < data.subTopics.length; i++) {
+      if (data.subTopics[i].isHidden === false) {
+        newData = newData + "\n" + " " + data.subTopics[i].topicName;
+      }
+      let subData = data.subTopics[i];
+      for (let j = 0; j < subData.subTopics.length; j++) {
+        if (subData.subTopics[j].isHidden === false) {
+          newData = newData + "\n" + "  " + subData.subTopics[j].topicName;
+        }
+        let subSubData = subData.subTopics[j];
+        for (let k = 0; k < subSubData.subTopics.length; k++) {
+          if (subSubData.subTopics[k].isHidden === false) {
+            newData =
+              newData + "\n" + "   " + subSubData.subTopics[k].topicName;
+          }
+        }
+      }
+    }
+    return newData;
+  };
+
+  const handleSave = () => {
+    let newData = handleCreateData(topicsData);
+    const blob = new Blob([newData], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "curriculum.txt";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  };
+
   return (
     <div className={styles["headerContainer"]}>
       <div className={styles["mainHeader"]}>
@@ -16,14 +56,13 @@ const Header = () => {
         />
         <div className={styles["loadAndSave"]}>
           <button className={styles["loadButton"]}>
-            <input
-              type="file"
-              accept="application/json"
-            />
+            <input type="file" accept="application/json" />
             Load
           </button>
 
-          <button className={styles["saveButton"]}>Save</button>
+          <button onClick={handleSave} className={styles["saveButton"]}>
+            Save
+          </button>
         </div>
       </div>
 
